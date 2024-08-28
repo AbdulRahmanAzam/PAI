ab50f80e95227eb818c0c8f2697c3de39665e7cd
Abdul Rahman Azam <69141440+AbdulRahmanAzam@users.noreply.github.com>
2024-08-28 10:07:33 +0500

    Create task2.py

---
+++ lab 2/task2.py
@@ -0,0 +1,10 @@
+name = "Abdulrahmanazam"
+
+last_letter = name[-1]
+
+if(last_letter == 'a' or last_letter == 'e' or last_letter == 'i' or last_letter == 'o' or last_letter == 'u'):
+    print("It is vowel")
+else:
+    print("It is consonant")
+
+    
