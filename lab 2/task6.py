b7d84165863d5c4052c37903ee740a5f402905b4
Abdul Rahman Azam <69141440+AbdulRahmanAzam@users.noreply.github.com>
2024-08-28 10:24:50 +0500

    Update task6.py

--- lab 2/task6.py
+++ lab 2/task6.py
@@ -1,3 +1,12 @@
+from functools import reduce
+import math
+
 l = [1,2,3,4,5]
 
-print(sum(l))
+res = reduce((lambda a, b: a * b), l)
+
+res = math.prod(l)
+
+res = eval('*'.join(map(str, l)))
+
+print(res)
