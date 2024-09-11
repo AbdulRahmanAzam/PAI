168615cf8cff270e0a834421906fba9f672e1f54
Abdul Rahman Azam <69141440+AbdulRahmanAzam@users.noreply.github.com>
2024-09-11 08:40:48 +0500

    Create 3.py

---
+++ lab 4/3.py
@@ -0,0 +1,13 @@
+class Student:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+        
+    def Print_biodata(self):
+        print(self.name, self.age)
+        
+
+s1 = Student("wow", "awesome")
+s1.Print_biodata()
+    
+  
