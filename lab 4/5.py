30f648bfc2e2d82d509c2519cfa76b32e0b6ecb0
Abdul Rahman Azam <69141440+AbdulRahmanAzam@users.noreply.github.com>
2024-09-11 09:25:32 +0500

    Create 5.py

---
+++ lab 4/5.py
@@ -0,0 +1,18 @@
+class Restaurant:
+    def __init__(self):
+        self.menu_items = []
+        self.book_table
+        self.customer_orders
+        
+    def add_item_to_menu(self, item):
+        self.menu_items.append(item)
+        
+    def book_tables(self, table):
+        self.book_table.append(table)
+        
+    def customer_order(self):
+        
+        
+    
+r = Restaurant()
+print(r.menu_items)
