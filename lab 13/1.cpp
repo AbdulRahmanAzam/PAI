656533ed8209c88d383a70a82bdfe155ba282e67
Abdul Rahman Azam <69141440+AbdulRahmanAzam@users.noreply.github.com>
2024-12-01 01:34:16 +0500

    Update 1.cpp

--- lab 13/1.cpp
+++ lab 13/1.cpp
@@ -1,1 +1,15 @@
-import
+calculate_grade <- function(score){
+    if(score >= 90){
+        grade <- "A"
+    }else if(score >= 80){
+        grade <- "B"
+    }else if(score >= 70){
+        grade <- "C"
+    }else {
+        grade <- "Fail"
+    }
+    return(grade)
+}
+
+score <- 85
+cat("Percentage: ", score, " Grade: ", calculate_grade(score))
